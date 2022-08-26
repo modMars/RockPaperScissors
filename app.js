3c2c44a15f956e531b2db6271c8efcfe2b8a1004
modMars <malejandrosalcido@gmail.com>
2022-08-26 15:24:41 -0600

    Completed getComputerChoice()

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 function getComputerChoice() {
-  console.log(Math.floor(Math.random() * (4 - 1) + 1));
+  let cpuChoice;
+  let cpuRand = Math.floor(Math.random() * (4 - 1) + 1);
+  if (cpuRand === 3) cpuChoice = 'Scissors!';
+  else if (cpuRand === 2) cpuChoice = 'Rock!';
+  else if (cpuRand === 1) cpuChoice = 'Paper!';
+  return cpuChoice;
 }
-getComputerChoice();
+console.log(getComputerChoice());
