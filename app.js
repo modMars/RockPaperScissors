28e05df1f195535f2ce51fae26649652203f4239
modMars <malejandrosalcido@gmail.com>
2022-08-29 02:29:12 -0600

    Add functionality to Rock button

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+  let cpuCounter = 0;
+  let playerCounter = 0;;
+
   function getComputerChoice() {
   let cpuChoice;
   let cpuRand = Math.floor(Math.random() * (4 - 1) + 1);
@@ -10,13 +13,26 @@
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === 'rock' && computerSelection === 'paper')
+  {
+    cpuCounter++;
     return 'You lose! paper beats rock!';
+  }
   else if (playerSelection === 'scissors' && computerSelection === 'rock')
+  {
+    cpuCounter++;
     return 'You lose! rock beats scissors!';
+  }
   else if (playerSelection === 'paper' && computerSelection === 'scissors')
+  {
+    cpuCounter++;
     return 'You lose! scissors beats paper!';
+  }
   else if (playerSelection === computerSelection) return "It's a tie!";
-  else return `You've won! ${playerSelection} beats ${computerSelection}`;
+  else 
+  {
+    playerCounter++;
+    return `You've won! ${playerSelection} beats ${computerSelection}`;
+  }
 }
 
 // function game() {
@@ -31,9 +47,18 @@ function playRound(playerSelection, computerSelection) {
 const btnRock = document.querySelector('.rck');
 const btnPaper = document.querySelector('.ppr');
 const btnScissors = document.querySelector('.scs');
+const container = document.querySelector('.container');
+const score = document.createElement('div');
 
 btnRock.addEventListener('click', () => {
-  console.log(playRound('rock', getComputerChoice()))}
+  score.textContent = (playRound('rock', getComputerChoice()))
+  console.log(score.textContent)
+  console.log(playerCounter, cpuCounter)
+  container.append(score.textContent);
+  setTimeout(score.textContent = "", 1000)
+  container.append(score.textContent)
+}
+  
 )
 btnPaper.addEventListener('click', () => {
   console.log(playRound('paper', getComputerChoice()))}
@@ -43,3 +68,4 @@ btnScissors.addEventListener('click', () => {
   console.log(playRound('scissors', getComputerChoice()))
 }
 )
+
